f869b918d8da49e76b280795a3d440156ddc61cf
yash.d.habib1@gmail.com <yash.habib@lief.care>
2025-03-02 15:37:09 +0530

    adding root route

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", chatRoutes);
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
